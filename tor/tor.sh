c7700fab67f3f1145432e7571945a14e5a8e935e
Your Name <you@example.com>
2014-08-28 21:57:58 +0000

    modified:   tor/tor.sh

--- tor/tor.sh
+++ tor/tor.sh
@@ -10,6 +10,6 @@ touch       /var/log/tor
 chmod 0002  /var/log/tor
 
 mkdir -p /var/run/polipo
-chmod -R 0002 /var/run/polipo
+chmod -R 0777 /var/run/polipo
 touch /var/log/polipo
 chmod 0002  /var/log/polipo
