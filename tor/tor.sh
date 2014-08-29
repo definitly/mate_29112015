1c4144c32fabe3fc73591bacc4c5c76f15e901f8
Your Name <you@example.com>
2014-08-29 23:20:03 +0000

    modified:   tor/tor.sh

--- tor/tor.sh
+++ tor/tor.sh
@@ -5,7 +5,8 @@ chmod 00444 /usr/local/etc/tor/torrc
 cp  $CURRENTDIRECTORY/config  /usr/local/etc/polipo/
 
 #tor create log file
-
+mkdir -p /var/run/tor
+chmod -R 0777 /var/run/tor
 touch       /var/log/tor
 chmod 0002  /var/log/tor
 
