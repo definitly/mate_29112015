e344bff740b08e2b443884016e7eba2d77f377b8
Your Name <you@example.com>
2014-08-29 13:03:18 +0000

    deleted:    monitoring/cpurrd.sh 	deleted:    monitoring/gr_cpuload.sh 	renamed:    monitoring/gr_coretemp.sh -> monitoring/gr_create.sh 	modified:   monitoring/m.sh 	modified:   monitoring/rrdcreate.sh

--- monitoring/gr_create.sh
+++ monitoring/gr_create.sh
@@ -48,3 +48,44 @@ GPRINT:core1:MAX:"Max %1.0lf deg, C " \
 GPRINT:core1:AVERAGE:"Avg %1.0lf deg, C " \
 GPRINT:core1:MIN:"Min %1.0lf deg, C \c" \
 COMMENT:"\n"
+
+
+
+#Picture CPU 12 hours
+
+CPUBASE="/var/db/cpuload.rrd"
+WWWPREFIX="/home/definitly"
+RRDCMD="/usr/local/bin/rrdtool"
+$RRDCMD graph $WWWPREFIX/cpuload.png \
+--width 580 \
+--height 250 \
+--imgformat PNG \
+--start -12h \
+--end now \
+--slope-mode \
+--font-render-mode light \
+--font DEFAULT:8:Tahoma \
+--font TITLE:9:Arial \
+--font AXIS:7:Arial \
+--font UNIT:8:"Courier New" \
+--font LEGEND:7:"Courier New" \
+--font WATERMARK:1:Arial \
+--color BACK#2F2F2F \
+--color CANVAS#003333 \
+--color SHADEA#CCCCCC \
+--color SHADEB#CCCCCC \
+--color FONT#FFFFFF \
+--color AXIS#FFFFFF \
+--color ARROW#FF0000 \
+--color GRID#CCCCCC \
+--color MGRID#CCCCCC \
+--vertical-label "Idle, %" \
+--title "Idle from last 12 hours" \
+DEF:idle=$CPUBASE:idle:LAST \
+LINE3:idle#FFFF00:"CPU idle " \
+AREA:idle#FF0000:"" \
+GPRINT:idle:LAST:"Last %.2lf %% idle" \
+GPRINT:idle:MAX:"Max %.2lf %% idle" \
+GPRINT:idle:AVERAGE:"Avg %.2lf %% idle " \
+GPRINT:idle:MIN:"Min  %.2lf %% idle \c" \
+COMMENT:"\n"
