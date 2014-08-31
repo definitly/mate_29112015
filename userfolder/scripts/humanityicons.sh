6d303338a9613f47d11b740e92b5b648ca646a48
Your Name <you@example.com>
2014-08-31 19:41:00 +0000

    new file:   userfolder/scripts/fftor.sh 	deleted:    userfolder/scripts/humanityicons.sh 	deleted:    userfolder/scripts/icons.sh 	deleted:    userfolder/scripts/winicons.sh 	deleted:    userfolder/scripts/woodicons.sh

--- userfolder/scripts/humanityicons.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget  --content-disposition      https://www.cubby.com/pl/humanityicons.tar.gz/_8b989777f31c48ec905c7dd311fd9750
-tar -xf   humanityicons.tar.gz       -C ~/
-rm humanityicons.tar.gz
-
-
-
