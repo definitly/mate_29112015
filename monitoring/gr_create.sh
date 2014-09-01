c84d74a12f2485498e56955a36dfa8ff901ecc88
Your Name <you@example.com>
2014-09-01 11:00:39 +0000

    modified:   monitoring/gr_create.sh 	modified:   monitoring/mon 	modified:   userfolder/.scripts/gr_create.sh

--- monitoring/gr_create.sh
+++ monitoring/gr_create.sh
@@ -96,3 +96,73 @@ GPRINT:user:MAX:"Max %.2lf" \
 GPRINT:user:AVERAGE:"Avg %.2lf" \
 GPRINT:user:MIN:"Min %.2lf \c" \
 COMMENT:"\n"
+
+
+
+CPUBASE="/var/db/memory.rrd"
+WWWPREFIX="/home/definitly"
+RRDCMD="/usr/local/bin/rrdtool"
+$RRDCMD graph $WWWPREFIX/memory.png \
+--width 580 \
+--height 250 \
+--imgformat PNG \
+--start -12h \
+--end now \
+--slope-mode \
+--font-render-mode light \
+--font DEFAULT:8:Tahoma \
+--font TITLE:9:Arial \
+--font AXIS:7:Arial \
+--font UNIT:8:"Courier New" \
+--font LEGEND:7:"Courier New" \
+--font WATERMARK:1:Arial \
+--color BACK#2F2F2F \
+--color CANVAS#003333 \
+--color SHADEA#CCCCCC \
+--color SHADEB#CCCCCC \
+--color FONT#FFFFFF \
+--color AXIS#FFFFFF \
+--color ARROW#FF0000 \
+--color GRID#CCCCCC \
+--color MGRID#CCCCCC \
+--vertical-label "Idle, %" \
+--title "Idle from last 12 hours" \
+DEF:wired=$CPUBASE:wired:LAST \
+DEF:active=$CPUBASE:active:LAST \
+DEF:inactive=$CPUBASE:inactive:LAST \
+DEF:free=$CPUBASE:free:LAST \
+LINE6:wired#FFFF00:"CPU idle " \
+AREA:wired#FF0000:"" \
+GPRINT:wired:LAST:"Last %5.1lf %% idle" \
+GPRINT:wired:MAX:"Max %5.1lf %% idle" \
+GPRINT:wired:AVERAGE:"Avg %5.1lf %% idle " \
+GPRINT:wired:MIN:"Min  %5.1lf %% idle \c" \
+COMMENT:"\s" \
+LINE5:active#FFFF00:"CPU idle " \
+AREA:active#FF0000:"" \
+GPRINT:active:LAST:"Last %5.1lf %% idle" \
+GPRINT:active:MAX:"Max %5.1lf %% idle" \
+GPRINT:active:AVERAGE:"Avg %5.1lf %% idle " \
+GPRINT:active:MIN:"Min  %5.1lf %% idle \c" \
+COMMENT:"\s" \
+LINE4:inactive#FFFF00:"CPU idle " \
+AREA:inactive#FF0000:"" \
+GPRINT:inactive:LAST:"Last %5.1lf %% idle" \
+GPRINT:inactive:MAX:"Max %5.1lf %% idle" \
+GPRINT:inactive:AVERAGE:"Avg %5.1lf %% idle " \
+GPRINT:inactive:MIN:"Min  %5.1lf %% idle \c" \
+COMMENT:"\s" \
+LINE2:free#00FF00:"CPU user" \
+GPRINT:free:LAST:"Last %5.1lf" \
+GPRINT:free:MAX:"Max %5.1lf" \
+GPRINT:free:AVERAGE:"Avg %5.1lf" \
+GPRINT:free:MIN:"Min %5.1lf \c" \
+COMMENT:"\n"
+
+
+
+
+
+
+
+
