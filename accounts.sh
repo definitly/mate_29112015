11935f5471f1f3f1529859e7adc2bf95e494cf7d
Your Name <you@example.com>
2014-09-03 10:43:20 +0000

    modified:   accounts.sh 	modified:   userfolder/note

--- accounts.sh
+++ accounts.sh
@@ -3,9 +3,10 @@
 
 passwd=639639
 login=definitly
-
+passwd1=639639
+login1=ff
 
 #create accounts
-#              echo $passwd1 | pw useradd $login1 -m -g wheel -s /bin/csh -h 0
+              echo $passwd1 | pw useradd $login1 -m -g wheel -s /bin/csh -h 0
              echo $passwd | pw useradd $login -m -g wheel -s /bin/csh -h 0
  
