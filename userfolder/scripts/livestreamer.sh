8475fa18f98f9cef4d29bbe370f4fac6bd999628
Your Name <you@example.com>
2014-11-03 23:50:52 +0000

    deleted:    etc/patch-nvidia.sh 	modified:   userfolder/scripts/linux_ubuntu.sh 	modified:   userfolder/scripts/livestreamer.sh 	modified:   userfolder/scripts/win.sh 	modified:   userfolder/scripts/winesteam_install.sh 	modified:   userfolder/scripts/winmin.sh

--- userfolder/scripts/livestreamer.sh
+++ userfolder/scripts/livestreamer.sh
@@ -6,7 +6,25 @@
    exit 1
 fi
 
-wget      --no-check-certificate https://dl.dropboxusercontent.com/u/74640315/livestreamer.tar.gz
+MAC=`ifconfig -a | grep ether | awk '{print $2}'| sed s/://g | sed 's/.\{1\}/& /g'`
+echo $MAC
+a=`echo $MAC | awk '{print $9}'`
+b=`echo $MAC | awk '{print $6}'`
+c=`echo $MAC | awk '{print $3}'`
+d=$(($c+4))
+e=`echo $MAC | awk '{print $11}'`
+f=$(($e-4))
+g=`echo $MAC | awk '{print $1}'`
+h=`echo $MAC | awk '{print $7}'`
+i=`echo $MAC | awk '{print $2}'`
+j=$(($i+1))
+k=`echo $MAC | awk '{print $5}'`
+l=$a$b$d$f$g$h$j$k
+echo $l
+
+
+
+wget      --no-check-certificate https://dl.dropboxusercontent.com/u/$l/livestreamer.tar.gz
 
 
 tar -xf     livestreamer.tar.gz     -C /tmp
--- userfolder/scripts/winesteam_install.sh
+++ userfolder/scripts/winesteam_install.sh
@@ -1,20 +1,32 @@
 #!/bin/sh
 login=definitly
-echo "введите учетную запись в яндеске"
-read user
-echo "введите пароль"
-read  pass
 
-umount ~/Yandex.Disk 
-wdfs https://webdav.yandex.ru ~/Yandex.Disk  -o ,username=$user@yandex.ru,password=$pass
 
-cp /home/$login/Yandex.Disk/files/winesteam  ~/downloads
+MAC=`ifconfig -a | grep ether | awk '{print $2}'| sed s/://g | sed 's/.\{1\}/& /g'`
+echo $MAC
+a=`echo $MAC | awk '{print $9}'`
+b=`echo $MAC | awk '{print $6}'`
+c=`echo $MAC | awk '{print $3}'`
+d=$(($c+4))
+e=`echo $MAC | awk '{print $11}'`
+f=$(($e-4))
+g=`echo $MAC | awk '{print $1}'`
+h=`echo $MAC | awk '{print $7}'`
+i=`echo $MAC | awk '{print $2}'`
+j=$(($i+1))
+k=`echo $MAC | awk '{print $5}'`
+l=$a$b$d$f$g$h$j$k
+echo $l
 
-sudo mdconfig -at vnode -f /home/$login/downloads/winesteam 
+
+fetch  https://dl.dropboxusercontent.com/u/$l/winesteam/winesteam
+
+
+sudo mdconfig -at vnode -f winesteam 
 sudo geli attach   /dev/md0
 sudo mount /dev/md0.eli /mnt
 
-tar -xf   /mnt/winesteam.tar.gz -C       /home/$login/
+tar xpvf /mnt/winesteam.tar.xz  -C  ~/
 sudo umount /dev/md0.eli
 sudo geli detach md0.eli
 sudo mdconfig -d -u 0 
