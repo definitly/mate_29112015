9ba74a6fda742d45801a43c57f12a0351fa0a3e9
Your Name <you@example.com>
2014-12-20 19:07:02 +0000

    modified:   userfolder/scripts/chrome.sh

--- userfolder/scripts/chrome.sh
+++ userfolder/scripts/chrome.sh
@@ -2,6 +2,6 @@
 
 wget --content-disposition    --no-check-certificate https://www.cubbyusercontent.com/pl/chromium.tar.gz/_4978f91d00614a0ca8041ac22fd67dbf
 
-tar -zxvf chromium.tar.gz  -C  /home/.config
+tar -zxvf chromium.tar.gz  -C  ~/.config
 
 rm chromium.tar.gz 
