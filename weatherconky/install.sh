98536eb827315cf07e7e6babe20a800497fbf12f
Your Name <you@example.com>
2015-01-02 19:52:20 +0300

    modified:   weatherconky/install.sh 	renamed:    weatherconky/.weatherconkyrc -> weatherconky/weatherconkyrc 	modified:   worldtime/timeconkyrc

--- weatherconky/install.sh
+++ weatherconky/install.sh
@@ -9,5 +9,5 @@ cp $CURRENTDIRECTORY/conkyweather           /usr/local/bin
 cp $CURRENTDIRECTORY/weather-on-off.sh      /usr/local/bin
 ln -s  /usr/local/lib/libxcb.so.1.1.0 /usr/local/lib/libxcb.so.2
 
-cp $CURRENTDIRECTORY/.weatherconkyrc        /home/$login
+cp $CURRENTDIRECTORY/weatherconkyrc        /home/$login/.weatherconkyrc
 chown  -R      $login:wheel                 /home/$login/.weatherconkyrc
