ea9d3ae13e0c314297ab4533bc3b05a587138373
Your Name <you@example.com>
2015-01-02 18:49:20 +0300

    modified:   worldtime/install.sh

--- worldtime/install.sh
+++ worldtime/install.sh
@@ -10,4 +10,4 @@ cp $CURRENTDIRECTORY/timeconky-on-off.sh      /usr/local/bin
 
 
 cp $CURRENTDIRECTORY/timeconkyrc        /home/$login/.timeconkyrc
-chown  -R      $login:wheel                 /home/$login/.weatherconkyrc
+chown  -R      $login:wheel                 /home/$login/.timeconkyrc
