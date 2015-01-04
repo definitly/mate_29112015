c4358014a5769e6c734797dc004520a60d22f313
Your Name <you@example.com>
2015-01-04 13:49:06 +0300

    renamed:    dzenconky/.dzenconkyrc -> dzenconky/dzenconkyrc 	modified:   dzenconky/install.sh

--- dzenconky/install.sh
+++ dzenconky/install.sh
@@ -7,6 +7,6 @@ CURRENTDIRECTORY=$(pwd)
 
 cp $CURRENTDIRECTORY/switchbackground      /usr/local/bin
 cp $CURRENTDIRECTORY/dzenconky             /usr/local/bin
-cp $CURRENTDIRECTORY/.dzenconkyrc          /home/$login
+cp $CURRENTDIRECTORY/dzenconkyrc          /home/$login/.dzenconkyrc
 
 cp $CURRENTDIRECTORY/dzen2-on-off          /usr/local/bin
