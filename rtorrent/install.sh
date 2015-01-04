a6f840feb62c31733dac700472a6bcb83de1a16c
Your Name <you@example.com>
2015-01-04 13:50:26 +0300

    modified:   rtorrent/install.sh 	renamed:    rtorrent/.rtorrent.rc -> rtorrent/rtorrent.rc

--- rtorrent/install.sh
+++ rtorrent/install.sh
@@ -6,7 +6,7 @@ CURRENTDIRECTORY=$(pwd)
                       cp  $CURRENTDIRECTORY/rtorrent  /usr/local/etc/rc.d
                       chmod 744 /usr/local/etc/rc.d/rtorrent
                   
-                      cp $CURRENTDIRECTORY/.rtorrent.rc  /home/$login
+                      cp $CURRENTDIRECTORY/rtorrent.rc  /home/$login/.rtorrent.rc
                          
                              mkdir /home/$login/.rtsession
                              chmod -R  777  /home/$login/.rtsession
