82d14aab0a850117c6ada6e57c65bb8a80ab9a9e
Your Name <you@example.com>
2015-02-13 08:37:39 +0300

    modified:   userfolder/scripts/fftor.sh

--- userfolder/scripts/fftor.sh
+++ userfolder/scripts/fftor.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo -u ff -H wget --content-disposition    --no-check-certificate  https://www.cubbyusercontent.com/pl/mozilla.tar.gz/_a4dc1c1f0dc745638e6e65cf0c31d7a0
+sudo  wget --content-disposition    --no-check-certificate  https://www.cubbyusercontent.com/pl/mozilla.tar.gz/_a4dc1c1f0dc745638e6e65cf0c31d7a0
 
-sudo -u ff -H tar -zxvf mozilla.tar.gz -C  /home/ff
+sudo tar -zxvf mozilla.tar.gz -C  /home/ff
 sudo rm mozilla.tar.gz 
