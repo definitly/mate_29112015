fa360367c1820bd2f51f97b74a2cf76d88c77a16
Your Name <you@example.com>
2015-04-11 16:45:29 +0300

    new file:   userfolder/scripts/vb5_install.sh

---
+++ userfolder/scripts/vb5_install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+ if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+ 
+  
+
+pkg update
+pkg install -r virtualbox5 -Uy virtualbox-ose-devel 
+pkg install -r FreeBSD -Uy tigervnc
+
+
+
+
+
+  kldload vboxdrv.ko
+  kldload vboxnetadp.ko
