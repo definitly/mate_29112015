b8328e14ff1213cbf5f3867131e5137a321eb423
Your Name <you@example.com>
2015-04-14 18:20:01 +0300

    modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -4,6 +4,6 @@ sudo cp skype.conf /usr/local/etc/pkg/repos
 sudo pkg update
 
 sudo pkg install -r skype4 -yU skype4
-sudo pkg delete -y linux-c6-pulseaudio-libs
+sudo pkg delete -f linux-c6-pulseaudio-libs
 cd 
 fetch  https://dl.dropboxusercontent.com/u/74640315/skype/skype_new
