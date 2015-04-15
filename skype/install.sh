48911318adb7815d025fbdc363f09a07403e1c49
Your Name <you@example.com>
2015-04-15 14:45:22 +0300

    modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -7,3 +7,12 @@ sudo pkg install -r skype4 -yU skype4
 sudo pkg delete -yf linux-c6-pulseaudio-libs
 cd 
 fetch  https://dl.dropboxusercontent.com/u/74640315/skype/skype_new
+
+
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/skype-4.2.0.13,1.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-lib-1.0.22.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-plugins-oss-1.0.21.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
+#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
