610545ae315e31b907f30cdbcc69970e6e4b68ec
Your Name <you@example.com>
2015-04-15 18:50:40 +0300

    modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-sudo cp skype.conf /usr/local/etc/pkg/repos
-sudo pkg update
 
-sudo pkg install -r skype4 -yU skype4
-sudo pkg delete -yf linux-c6-pulseaudio-libs
-cd 
 fetch  https://dl.dropboxusercontent.com/u/74640315/skype/skype_new
 
 
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/skype-4.2.0.13,1.txz
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-lib-1.0.22.txz
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-plugins-oss-1.0.21.txz
-#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
-#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
-#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
-#fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
+fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
+sudo pkg add linux-c6-dbus-libs-1.2.24.txz
+fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
+sudo pkg add linux-c6-qt47-4.7.2.txz
+fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
+sudo pkg add linux-c6-qt47-webkit-4.7.2.txz
+fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
+sudo pkg add linux-c6-qt47-x11-4.7.2.txz
