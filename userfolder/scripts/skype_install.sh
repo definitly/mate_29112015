dceed6016c9b08036babb0209655747dddbf9340
Your Name <you@example.com>
2015-04-15 19:05:26 +0300

    modified:   userfolder/scripts/skype_install.sh

--- userfolder/scripts/skype_install.sh
+++ userfolder/scripts/skype_install.sh
@@ -6,11 +6,10 @@ if [ "$(id -u)" != "0" ]; then
    exit 1
 fi
 
-wget --content-disposition   https://www.cubby.com/pl/skype.tar.gz/_51650db679da4006a040e0a16cec6b7d
+wget --content-disposition   https://www.cubbyusercontent.com/pl/skype-devel.tar.gz/_500bfa48a69947fc9388f3840f050a71
 
-tar zxvf skype.tar.gz
-rm skype.tar.gz
-cd skype
-pkg add skype-2.1.0.81_1,1.txz
+tar zxvf skype-devel.tar.gz
+rm skype-devel.tar.gz
+cd skype-devel
+pkg add skype-2.2.0.35.txz
 cd ..
-rm -R skype
