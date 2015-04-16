872b14530641ddac2476398216f1ec2c43941317
Your Name <you@example.com>
2015-04-16 18:03:39 +0300

    modified:   skype/install.sh 	modified:   skype/skype

--- skype/install.sh
+++ skype/install.sh
@@ -2,8 +2,7 @@
 
 
 fetch  https://dl.dropboxusercontent.com/u/74640315/skype/skype_new
-
-
+chmod +x skype_new
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/skype-4.2.0.13,1.txz
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-lib-1.0.22.txz
 #fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-alsa-plugins-oss-1.0.21.txz
@@ -15,3 +14,6 @@ fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webk
 sudo pkg add linux-c6-qt47-webkit-4.7.2.txz
 fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
 sudo pkg add linux-c6-qt47-x11-4.7.2.txz
+
+fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
+sudo pkg add linux-c6-qt47-webkit-4.7.2.txz
