073becf639ec785f8bfede848ef9972b2c78fd2f
Your Name <you@example.com>
2015-04-23 17:27:47 +0300

    modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -22,8 +22,9 @@ cd /tmp
                   sudo  pkg install -r skype-devel -yU skype-devel
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-dbus-libs-1.2.24_1.txz
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-4.7.2_1.txz
-                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-webkit-4.7.2_1.txz
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-x11-4.7.2_1.txz
+                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-webkit-4.7.2_1.txz
+                  
 
                   #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
                   #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
