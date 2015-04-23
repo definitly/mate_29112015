96ff2d42595a1121c2698ac00092f063185a1709
Your Name <you@example.com>
2015-04-23 12:25:19 +0300

    modified:   userfolder/scripts/swf/echomsk.sh

--- userfolder/scripts/swf/echomsk.sh
+++ userfolder/scripts/swf/echomsk.sh
@@ -7,7 +7,7 @@ app="echomsk/_definst_/"
 
 flashVer="LNX 11,2,202,341"
 swfUrl="http://echomsk.onlinetv.ru/play.swf?t=23346498571&s=gs83t2e71al1s6e5e9k9kppst6"
-tcUrl="rtmp://prague1.setevisor.tv:1935/echomsk/_definst_/"
+tcUrl="rtmp://cdn0.onlinetv.ru:1935/echomsk/_definst_/"
 pageUrl="amf.string"
 play="echomsk.stream?cid=0"
 
