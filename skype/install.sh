e2680426edc3a04d8e489384ba8dce34b06c6f22
Your Name <you@example.com>
2015-04-23 17:32:31 +0300

    modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -20,6 +20,8 @@ cd /tmp
         #         sudo pkg add skype-devel-2.2.0.35_1.txz
 
                   sudo  pkg install -r skype-devel -yU skype-devel
+                  sudo pkg install -r skype-devel -yU linux-c6-libv4l linux-c6-libsigc++20 linux-c6-alsa-plugins-oss
+
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-dbus-libs-1.2.24_1.txz
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-4.7.2_1.txz
                   sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-x11-4.7.2_1.txz
