0623950ac03269d6044037afa402635fd163b7c7
Your Name <you@example.com>
2015-04-23 17:22:58 +0300

    modified:   etc/freebsd.conf 	modified:   skype/install.sh

--- skype/install.sh
+++ skype/install.sh
@@ -12,24 +12,25 @@ cd /tmp
                    fi 
 
 
-                 sudo   cp   skype_new  /usr/local/bin 
-                        wget   https://dl.dropboxusercontent.com/u/74640315/skype/skype-devel.tar.gz
-                        tar zxvf skype-devel.tar.gz
-                        rm skype-devel.tar.gz
-                        cd skype-devel
-                 sudo pkg add skype-devel-2.2.0.35_1.txz
+        #         sudo   cp   skype_new  /usr/local/bin 
+        #                wget   https://dl.dropboxusercontent.com/u/74640315/skype/skype-devel.tar.gz
+        #                tar zxvf skype-devel.tar.gz
+        #                rm skype-devel.tar.gz
+        #                cd skype-devel
+        #         sudo pkg add skype-devel-2.2.0.35_1.txz
 
+                  sudo  pkg install -r skype-devel -yU skype-devel
+                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-dbus-libs-1.2.24_1.txz
+                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-4.7.2_1.txz
+                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-webkit-4.7.2_1.txz
+                  sudo  pkg add    https://bitbucket.org/vcore/skype-devel/src/abe0d75f4fa5648e563829b1b839c8d08e8b3082/linux-c6-qt47-x11-4.7.2_1.txz
 
+                  #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
+                  #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
+                  #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
+                  #   fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
 
-
-
-
-                     fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-dbus-libs-1.2.24.txz
-                     fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-4.7.2.txz
-                     fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-webkit-4.7.2.txz
-                     fetch https://dl.dropboxusercontent.com/u/74640315/xinitronix/linux-c6-qt47-x11-4.7.2.txz
-
-                 sudo pkg add linux-c6-dbus-libs-1.2.24.txz
-                 sudo pkg add linux-c6-qt47-4.7.2.txz
-                 sudo pkg add linux-c6-qt47-x11-4.7.2.txz
-                 sudo pkg add linux-c6-qt47-webkit-4.7.2.txz
+               #  sudo pkg add linux-c6-dbus-libs-1.2.24.txz
+               #  sudo pkg add linux-c6-qt47-4.7.2.txz
+               #  sudo pkg add linux-c6-qt47-x11-4.7.2.txz
+               #  sudo pkg add linux-c6-qt47-webkit-4.7.2.txz
