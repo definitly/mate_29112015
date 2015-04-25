3cd39cab0cd6a09d66a093d139baf06ca47a23c4
Your Name <you@example.com>
2015-04-25 12:44:26 +0300

    modified:   userfolder/scripts/swf/liveall.sh

--- userfolder/scripts/swf/liveall.sh
+++ userfolder/scripts/swf/liveall.sh
@@ -3,18 +3,16 @@
 
 
 
-app="sliveall/"
+app="liveall/"
 
 flashVer="LNX 11,2,202,451"
 swfUrl="http://wds.liveall.tv/jwplayer.flash.swf"
-tcUrl="rtmp://31.220.0.12/sliveall/"
+tcUrl="rtmp://50.22.223.47/liveall/"
 pageUrl="http://liveall.tv/player.php?streampage=z38ww&width=728&height=480"
-play="z38ww?keys=aMi-n_CV3Hdqm815J_ivKg&keyt=1428843403"
+play="mulsi59807eestoofk?keys=QFs_H0XPKku8W_v1Tg9BFw&keyt=1429969101"
 
 
 
 
-rtmpdump -r $tcUrl -a $app -f $flashVer -W $swfUrl -p $pageUrl  -y  $play   -o "1.flv"
-
-
+rtmpdump -r $tcUrl -a $app -f $flashVer -W $swfUrl -p $pageUrl  -y  $play  | mplayer -cache 2048 -cache-min 30 -noautosub -
 
