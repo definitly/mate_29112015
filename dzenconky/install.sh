a43f500019d5975fadefbad83e343827383025cd
Your Name <you@example.com>
2015-04-28 22:46:33 +0300

    modified:   dzenconky/dzenconkyrc 	modified:   dzenconky/install.sh

--- dzenconky/install.sh
+++ dzenconky/install.sh
@@ -7,6 +7,8 @@ CURRENTDIRECTORY=$(pwd)
 
 cp $CURRENTDIRECTORY/switchbackground      /usr/local/bin
 cp $CURRENTDIRECTORY/dzenconky             /usr/local/bin
-cp $CURRENTDIRECTORY/dzenconkyrc          /home/$login/.dzenconkyrc
+cp $CURRENTDIRECTORY/dzenconkyrc           /home/$login/.dzenconkyrc
+chown  -R      $login:wheel                /home/$login/.dzenconkyrc
+
 
 cp $CURRENTDIRECTORY/dzen2-on-off          /usr/local/bin
