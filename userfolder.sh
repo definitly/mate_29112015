56fc82136709c98f08908ea144d9c7c6f71e26b4
Your Name <you@example.com>
2015-05-02 16:50:55 +0300

    modified:   userfolder.sh

--- userfolder.sh
+++ userfolder.sh
@@ -47,6 +47,6 @@ login=definitly
 
 #/home/$login/.login  
                   
-#                      echo 'startx' >>                                                              /home/$login/.login 
+#                      echo 'startx'                                                  >>            /home/$login/.login 
 
                         
