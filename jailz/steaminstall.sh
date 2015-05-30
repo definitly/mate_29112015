04139935ad7ef3636a60501381c1bcefde85fa6b
Your Name <you@example.com>
2015-05-30 14:55:50 +0300

    изменено:      jailz/steaminstall.sh

--- jailz/steaminstall.sh
+++ jailz/steaminstall.sh
@@ -4,5 +4,5 @@ sudo mv /compat/linux /
 fetch http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
 mkdir ubuntu
 tar -zxvf     ubuntu-12.04-x86.tar.gz         -C  ubuntu
-cp /etc/resolv.conf  /ubuntu/etc
+cp /etc/resolv.conf  ubuntu/etc
 sudo cp -R ubuntu /compat
