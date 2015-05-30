f1db7c9ab04fec6a5dbc692b74a836d7b3244482
Your Name <you@example.com>
2015-05-30 13:52:03 +0300

    новый файл:    jailz/steaminstall.sh 	изменено:      jailz/ubuntu12.04

---
+++ jailz/steaminstall.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo mv /compat/linux /
+fetch http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+mkdir ubuntu
+tar -zxvf     ubuntu-12.04-x86.tar.gz         -C  ubuntu
+cp /etc/resolv.conf  /ubuntu/etc
+sudo cp -R ubuntu /compat
