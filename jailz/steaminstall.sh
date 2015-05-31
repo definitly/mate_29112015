8b8076a138f8d52fdd13ec5c4a2bb5e86127c0ff
Your Name <you@example.com>
2015-05-31 17:53:14 +0300

    изменено:      jailz/steaminstall.sh

--- jailz/steaminstall.sh
+++ jailz/steaminstall.sh
@@ -6,5 +6,7 @@ mkdir ubuntu
 tar -zxvf     ubuntu-12.04-x86.tar.gz         -C  ubuntu
 cp /etc/resolv.conf  ubuntu/etc
 cp ubuntu.sh   ubuntu/root
+cp libflashsupport.so ubuntu/usr/lib
 rm  ubuntu/var/run
+chmod 777 /ubuntu/tmp
 sudo cp -R ubuntu /compat
