c6a54b154531fd2a17921172dd2d1726511cb655
Your Name <you@example.com>
2015-05-31 10:13:54 +0300

    удалено:       1 	изменено:      jailz/steaminstall.sh 	изменено:      jailz/ubuntu12.04

--- jailz/steaminstall.sh
+++ jailz/steaminstall.sh
@@ -5,4 +5,5 @@ fetch http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
 mkdir ubuntu
 tar -zxvf     ubuntu-12.04-x86.tar.gz         -C  ubuntu
 cp /etc/resolv.conf  ubuntu/etc
+rm  ubuntu/var/run
 sudo cp -R ubuntu /compat
