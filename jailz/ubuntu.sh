9a30616447f55787cfb626cd266c34b486193a61
Your Name <you@example.com>
2015-06-02 18:29:26 +0300

    изменено:      jailz/ubuntu.sh

--- jailz/ubuntu.sh
+++ jailz/ubuntu.sh
@@ -1,6 +1,7 @@
 dpkg-divert --local --rename --add /sbin/initctl
 ln -s /bin/true /sbin/initctl
 chown -R man:root /var/cache/man
+dpkg -i pulseaudio_1.1-1_i386.deb
 locale-gen en_US.UTF-8
 locale-gen ru_RU.UTF-8
 dpkg-reconfigure locales
