f8be0f64683e0fc3602a53d671155db3d1ab6fb6
Your Name <you@example.com>
2015-06-02 14:09:51 +0300

    изменено:      jailz/steaminstall.sh

--- jailz/steaminstall.sh
+++ jailz/steaminstall.sh
@@ -1,12 +1,29 @@
 #!/bin/sh
 
 sudo mv /compat/linux /
-fetch http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+
+  
+      if ! [ -f  ubuntu-12.04-x86.tar.gz  ]; then
+                  
+   fetch http://download.openvz.org/template/precreated/ubuntu-12.04-x86.tar.gz
+fi
+
+
+
+
 mkdir ubuntu
 tar -zxvf     ubuntu-12.04-x86.tar.gz         -C  ubuntu
 cp /etc/resolv.conf  ubuntu/etc
 cp ubuntu.sh   ubuntu/root
 cp libflashsupport.so ubuntu/usr/lib
+
+      if ! [ -f  pulseaudio_1.1-1_i386.deb  ]; then
+                  
+   wget https://github.com/definitly/pulseaudio/raw/master/pulseaudio_1.1-1_i386.deb
+fi
+
+
+cp pulseaudio_1.1-1_i386.deb ubuntu/root
 rm  ubuntu/var/run
 chmod 777 ubuntu/tmp
-sudo cp -R ubuntu /compat
+#sudo cp -R ubuntu /compat
