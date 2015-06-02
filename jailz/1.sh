2204ffe190a7c9d083f719d9e9dff3f98b06f59e
Your Name <you@example.com>
2015-06-02 05:54:26 +0300

    новый файл:    jailz/1.sh

---
+++ jailz/1.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+
+    while true; do 
+
+sleep 3;
+rm /home/definitly/.steam/steam.pid
+rm /home/definitly/.steam/steam.pipe
+rm /home/definitly/.steam/registry.vdf
+rm -R ~/.nv
+rm -R ~/.pulse
+rm -R ~/.pki
+/home/definitly/.local/share/Steam/ubuntu12_32/steam 
+
+     done 
