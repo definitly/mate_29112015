6ded4bbc1b2010956686d7624eb5dcb2187b596c
Your Name <you@example.com>
2015-06-03 05:32:07 +0300

    изменено:      jailz/1.sh

--- jailz/1.sh
+++ jailz/1.sh
@@ -4,13 +4,13 @@
 
     while true; do 
 
-sleep 3;
-rm /home/definitly/.steam/steam.pid
-rm /home/definitly/.steam/steam.pipe
-rm /home/definitly/.steam/registry.vdf
-rm -R ~/.nv
-rm -R ~/.pulse
-rm -R ~/.pki
-/home/definitly/.local/share/Steam/ubuntu12_32/steam 
+sleep 0.1;
+#rm /home/definitly/.steam/steam.pid
+#rm /home/definitly/.steam/steam.pipe
+#rm /home/definitly/.steam/registry.vdf
+#rm -R ~/.nv
+#rm -R ~/.pulse
+#rm -R ~/.pki
+ /home/definitly/.local/share/Steam/ubuntu12_32/steam  
 
      done 
