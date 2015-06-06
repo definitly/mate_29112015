c9f5fd310c99d721652ea7526ccea38c79958c22
Your Name <you@example.com>
2015-06-06 13:40:08 +0300

    изменено:      userfolder/scripts/fftor.sh

--- userfolder/scripts/fftor.sh
+++ userfolder/scripts/fftor.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-sudo  wget --content-disposition    --no-check-certificate  https://www.cubbyusercontent.com/pl/mozilla.tar.gz/_a4dc1c1f0dc745638e6e65cf0c31d7a0
+#sudo  wget --content-disposition    --no-check-certificate  https://www.cubbyusercontent.com/pl/mozilla.tar.gz/_a4dc1c1f0dc745638e6e65cf0c31d7a0
+sudo  wget https://dl.dropboxusercontent.com/u/74640315/tor/mozilla.tar.gz
 sudo tar -zxvf mozilla.tar.gz -C  /home/ff
 sudo rm mozilla.tar.gz 
