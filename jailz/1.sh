23b3589639bfef7193b28a5800d6ce24de841da1
Your Name <you@example.com>
2015-06-06 06:32:26 +0300

    изменено:      jailz/1.sh 	новый файл:    jailz/steamrun 	изменено:      scripts/freebsd-custom/note

--- jailz/1.sh
+++ jailz/1.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
-
+rm count.log
 i=0
 
     while i=$(($i+1)) ; do 
 
 sleep 0.1;
 echo $i >> count.log
+
+
+       export LD_LIBRARY_PATH=/home/definitly/.local/share/Steam/ubuntu12_32
+
+       sudo chmod 1777 /compat/linux/tmp
+  
  /home/definitly/.local/share/Steam/ubuntu12_32/steam  
 
      done 
