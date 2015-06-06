071a6448d757a517c61c28af4d1661600e52d86e
Your Name <you@example.com>
2015-06-06 19:50:07 +0300

    изменено:      jailz/1.sh

--- jailz/1.sh
+++ jailz/1.sh
@@ -11,7 +11,9 @@ echo $i >> count.log
        export LD_LIBRARY_PATH=/home/definitly/.local/share/Steam/ubuntu12_32
 
        sudo chmod 1777 /compat/linux/tmp
-  
+        sudo  ln -s /var/run/shm /dev/shm
+        sudo  chmod 1777 /dev/shm
+    date | awk '{print $6}' >> count.log
  /home/definitly/.local/share/Steam/ubuntu12_32/steam  
 
      done 
