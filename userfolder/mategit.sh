29c1f39740c31ba94524c364270e9c5e40d9e6b6
Your Name <you@example.com>
2015-07-08 10:55:53 +0300

    изменено:      userfolder/mategit.sh

--- userfolder/mategit.sh
+++ userfolder/mategit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
    git clone git://github.com/definitly/mate.git
-   git clone git://github.com/definitly/weather.git
+#   git clone git://github.com/definitly/weather.git
    git clone git://github.com/definitly/themes.git
