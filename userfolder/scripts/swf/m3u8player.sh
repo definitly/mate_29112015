28884d5f5aa04170983f52879dd13fde31f065a3
Your Name <you@example.com>
2015-08-01 22:09:36 +0300

    изменено:      userfolder/scripts/swf/m3u8player.sh

--- userfolder/scripts/swf/m3u8player.sh
+++ userfolder/scripts/swf/m3u8player.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-if [ -z "$1" ]
-then
-  echo "Порядок использования: `basename $0` плейлист m3u8  "
-  exit 0
-fi
+sudo tcpdump -w file.tcpdump -s 0 -i ale0
+     host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8?" | head -n 1 | awk ' {print $1} ')
+     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8?" | head -n 1 | awk ' {print $3} ')
+     url=http://$host$GET
+echo $host
+echo $GET
+echo $url 
 
-
-
-mplayer -cache 1000 -cache-min 99 ffmpeg://$1
+mpv $url
