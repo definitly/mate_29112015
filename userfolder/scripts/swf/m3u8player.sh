64edeaccd9d5f9a1e593064d7b02b404d329d989
Your Name <you@example.com>
2015-08-05 09:57:55 +0300

    изменено:      userfolder/scripts/swf/m3u8player.sh

--- userfolder/scripts/swf/m3u8player.sh
+++ userfolder/scripts/swf/m3u8player.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo tcpdump -w file.tcpdump -s 0 -i ale0
-     host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8" | head -n 1 | awk ' {print $1} ')
-     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8" | head -n 1 | awk ' {print $3} ')
+     host=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8?" | head -n 1 | awk ' {print $1} ')
+     GET=$(tshark -r file.tcpdump -T fields -e http.host -e tcp.port -e http.request.uri "http&&http.request.uri" | grep "m3u8?" | head -n 1 | awk ' {print $3} ')
      url=http://$host$GET
 echo $host
 echo $GET
