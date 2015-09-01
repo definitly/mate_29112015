a8a5d7e3a0aa1ef5f0f453a728a53aa8aa921671
Your Name <you@example.com>
2015-09-01 09:11:26 +0300

    изменено:      userfolder/f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -56,7 +56,7 @@ sudo /home/$login/.scripts/rrdcreate.sh
 
 
 sudo pkg delete -y leafpad gsettings-desktop-schemas terminus-font
-sudo pkg install -y leafpad gsettings-desktop-schemas terminus-font
+sudo pkg install -y leafpad gsettings-desktop-schemas terminus-font tshark-lite
 sudo xset +fp /usr/local/share/fonts/terminus-font/
 
 
