d3877301bb097c93d3de8abb300cadde9d8e73ba
Your Name <you@example.com>
2015-09-04 22:48:41 +0300

    изменено:      rtorrent/install.sh 	изменено:      rtorrent/rutorrent.sh

--- rtorrent/install.sh
+++ rtorrent/install.sh
@@ -9,7 +9,12 @@ CURRENTDIRECTORY=$(pwd)
                       chmod 744                            /usr/local/etc/rc.d/rtorrent
                   
                       cp $CURRENTDIRECTORY/rtorrent.rc     /home/$login/.rtorrent.rc
-                         
+                      cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+                      cp $CURRENTDIRECTORY/php-fpm.conf    /usr/local/etc
+                      cp $CURRENTDIRECTORY/nginx.conf      /usr/local/etc/nginx
+                      cd /usr/local/www/ 
+                      git clone https://github.com/Novik/ruTorrent.git
+                      chown -R www:www /usr/local/www/ruTorrent
                              mkdir                         /home/$login/.rtsession
                              chmod -R  777                 /home/$login/.rtsession
                              mkdir                         /home/$login/downloads
