8af35be06552d6248cf2309c6021bfd3e5f226f9
Your Name <you@example.com>
2015-09-04 22:11:05 +0300

    изменено:      rtorrent/rtorrent 	изменено:      rtorrent/rutorrent.sh

--- rtorrent/rutorrent.sh
+++ rtorrent/rutorrent.sh
@@ -1,3 +1,10 @@
-git clone https://github.com/Novik/ruTorrent.git
+#!/bin/sh
+
 cp /usr/local/etc/php.ini-production /usr/local/etc/php.ini
+cp php-fpm.conf  /usr/local/etc
+cp nginx.conf    /usr/local/etc/nginx
+service nginx restart
+service php-fpm restart
+cd /usr/local/www/
+git clone https://github.com/Novik/ruTorrent.git
 chown -R www:www /usr/local/www/ruTorrent
