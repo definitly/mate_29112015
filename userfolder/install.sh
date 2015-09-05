68960bda9507aaa1101cff02f6ba06684567b8f9
Your Name <you@example.com>
2015-09-05 14:31:13 +0300

    изменено:      userfolder/install.sh

--- userfolder/install.sh
+++ userfolder/install.sh
@@ -4,3 +4,6 @@ sudo pkg delete -y leafpad
 sudo pkg install -y leafpad  tshark-lite devel/py-dateutil
 #sudo xset +fp /usr/local/share/fonts/terminus-font/
 git clone git://github.com/definitly/mate.git
+cd /usr/local/www/
+git clone https://github.com/Novik/ruTorrent.git
+chown -R www:www /usr/local/www/ruTorrent
