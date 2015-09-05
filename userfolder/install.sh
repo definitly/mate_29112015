4a599c324ae453d0a0b05d0cb7a28a3441e70fba
Your Name <you@example.com>
2015-09-05 08:30:14 +0300

    изменено:      userfolder/install.sh

--- userfolder/install.sh
+++ userfolder/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo pkg delete -y leafpad gsettings-desktop-schemas terminus-font
-sudo pkg install -y leafpad gsettings-desktop-schemas terminus-font tshark-lite devel/py-dateutil
-sudo xset +fp /usr/local/share/fonts/terminus-font/
+sudo pkg delete -y leafpad 
+sudo pkg install -y leafpad  tshark-lite devel/py-dateutil
+#sudo xset +fp /usr/local/share/fonts/terminus-font/
 git clone git://github.com/definitly/mate.git
