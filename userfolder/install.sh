68f2491637778451e337d5991a9be4afea878fae
Your Name <you@example.com>
2015-09-06 13:59:34 +0300

    изменено:      userfolder/install.sh

--- userfolder/install.sh
+++ userfolder/install.sh
@@ -7,4 +7,4 @@ git clone git://github.com/definitly/mate.git
 cd /usr/local/www/
 sudo git clone https://github.com/Novik/ruTorrent.git
 sudo chown -R www:www /usr/local/www/ruTorrent
-sudo cp mate/rtorrent/config.php     /usr/local/www/ruTorrent/conf
+sudo cp /home/definitly/mate/rtorrent/config.php     /usr/local/www/ruTorrent/conf
