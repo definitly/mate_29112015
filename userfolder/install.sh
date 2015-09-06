3c921cc55c206ab2d46995b7809213bb2ce2ceb7
Your Name <you@example.com>
2015-09-06 13:42:23 +0300

    новый файл:    rtorrent/config.php 	изменено:      rtorrent/rtorrent.rc

--- userfolder/install.sh
+++ userfolder/install.sh
@@ -7,3 +7,4 @@ git clone git://github.com/definitly/mate.git
 cd /usr/local/www/
 sudo git clone https://github.com/Novik/ruTorrent.git
 sudo chown -R www:www /usr/local/www/ruTorrent
+sudo cp mate/rtorrent/config.php     /usr/local/www/ruTorrent/conf
