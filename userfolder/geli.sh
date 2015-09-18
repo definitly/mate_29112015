8d45c0420c70f33faa81f3a2b25949ab4a95ebc2
Your Name <you@example.com>
2015-09-18 15:48:21 +0300

    изменено:      userfolder/geli.sh

--- userfolder/geli.sh
+++ userfolder/geli.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
+login=definitly
+
+MAC=`ifconfig -a | grep ether | awk '{print $2}'| sed s/://g | sed 's/.\{1\}/& /g'`
+echo $MAC      
+a=`echo $MAC | awk '{print $1}'`
+b=`echo $MAC | awk '{print $3}'`
+a=`echo 1$a$b | awk '{ printf("%c", $0); }'`
+c=`echo $MAC | awk '{print $6}'`
+c=`echo 11$c  | awk '{ printf("%c", $0); }'`
+d=`echo $MAC | awk '{print $12}'`
+h=`echo $MAC | awk '{print $11}'`
+i=`echo $MAC | awk '{print $11}'`
+j=$(($i+1))
+j=`echo $j$h  | awk '{ printf("%c", $0); }'`
+g=`echo $MAC | awk '{print $5}'`
+g=`echo 11$g | awk '{ printf("%c", $0); }'`
+k=`echo $MAC | awk '{print $1}'`
+k=`echo 1$k$k | awk '{ printf("%c", $0); }'`
+k=$a$c$d$d$j$g$k
+echo $k
 
 
-      
+
+
+
+
+
+
+
                      sudo mdconfig -at vnode -f $k
                      sudo geli attach  -j /tmp/passgeli /dev/md0
                      sudo mount /dev/md0.eli /mnt
