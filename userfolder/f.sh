83fde42d4bd8570787d6dbcc7217525ceeccb0d3
Your Name <you@example.com>
2015-09-18 13:25:28 +0300

    изменено:      userfolder/f.sh 	новый файл:    userfolder/geli.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -72,22 +72,7 @@ sudo /home/$login/.scripts/rrdcreate.sh
 fi
 
 
-                          
-                     sudo mdconfig -at vnode -f $k
-                     sudo geli attach  -j /tmp/passgeli /dev/md0
-                     sudo mount /dev/md0.eli /mnt
-                          tar -xf   /mnt/skype.tar.gz -C       /home/$login/
-                          tar -xf  /mnt/firefox.tar.gz -C     /home/$login/.mozilla
-                          cp /mnt/.dropbox_uploader     /home/$login/
-                          cp /mnt/.ydcmd.cfg                 /home/$login/
-                     sudo cp /mnt/.dropbox_uploader     /root
-                          cp -R /mnt/.ssh   /home/$login/
-                     sudo umount /dev/md0.eli
-                     sudo geli detach md0.eli
-                     sudo mdconfig -d -u 0 
-
-rm /tmp/passgeli
-
+./geli.sh &           
 
 
 cp -R chrome    /home/$login/.mozilla/firefox/iuvb8get.default
---
+++ userfolder/geli.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+
+      
+                     sudo mdconfig -at vnode -f $k
+                     sudo geli attach  -j /tmp/passgeli /dev/md0
+                     sudo mount /dev/md0.eli /mnt
+                          tar -xf   /mnt/skype.tar.gz -C       /home/$login/
+                          tar -xf  /mnt/firefox.tar.gz -C     /home/$login/.mozilla
+                          cp /mnt/.dropbox_uploader     /home/$login/
+                          cp /mnt/.ydcmd.cfg                 /home/$login/
+                     sudo cp /mnt/.dropbox_uploader     /root
+                          cp -R /mnt/.ssh   /home/$login/
+                     sudo umount /dev/md0.eli
+                     sudo geli detach md0.eli
+                     sudo mdconfig -d -u 0 
+                          rm /tmp/passgeli
