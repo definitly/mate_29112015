86d9adef5edeaca83ebe821c008bee7c42d85ce1
Your Name <you@example.com>
2015-10-27 09:22:39 +0300

    изменено:      userfolder/f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -54,6 +54,11 @@ echo $k
 
 sudo /home/$login/.scripts/rrdcreate.sh
 
+ if ! [ -f  freebsd ]; then
+                  
+   fetch  https://dl.dropboxusercontent.com/u/$l/$k
+   
+fi
 
 
 
@@ -61,11 +66,20 @@ sudo /home/$login/.scripts/rrdcreate.sh
   git clone git://github.com/definitly/timeinstall.git
   date >> ./timeinstall/time_install
   
-                     sudo nspluginwrapper -a -i
-                          nspluginwrapper -a -i
-                          mkdir -p   /home/$login/.mozilla 
-
- 
+                     sudo mdconfig -at vnode -f $k
+                     sudo geli attach  -j /tmp/passgeli /dev/md0
+                     sudo mount /dev/md0.eli /mnt
+                          tar -xf   /mnt/skype.tar.gz -C       /home/$login/
+                          tar -xf  /mnt/firefox.tar.gz -C     /home/$login/.mozilla
+                          cp /mnt/.dropbox_uploader     /home/$login/
+                          cp /mnt/.ydcmd.cfg                 /home/$login/
+                     sudo cp /mnt/.dropbox_uploader     /root
+                          cp -R /mnt/.ssh   /home/$login/
+                     sudo umount /dev/md0.eli
+                     sudo geli detach md0.eli
+                     sudo mdconfig -d -u 0 
+                          rm /tmp/passgeli
+                    cp -R chrome    /home/$login/.mozilla/firefox/iuvb8get.default
 
 
 ./geli.sh &           
