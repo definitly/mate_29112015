c22a21206cdcee7df6550a32f22d0b507d158399
Your Name <you@example.com>
2015-11-18 22:53:20 +0300

    изменено:      userfolder/f.sh

--- userfolder/f.sh
+++ userfolder/f.sh
@@ -54,40 +54,17 @@ echo $k
 
 sudo /home/$login/.scripts/rrdcreate.sh
 
- if ! [ -f  freebsd ]; then
-                  
-   fetch  https://dl.dropboxusercontent.com/u/$l/$k
-   
-fi
-
-
-
-
   git clone git://github.com/definitly/timeinstall.git
   date >> ./timeinstall/time_install
   
-                     sudo mdconfig -at vnode -f $k
-                     sudo geli attach  -j /tmp/passgeli /dev/md0
-                     sudo mount /dev/md0.eli /mnt
-                          tar -xf   /mnt/skype.tar.gz -C       /home/$login/
-                          tar -xf  /mnt/firefox.tar.gz -C     /home/$login/.mozilla
-                          cp /mnt/.dropbox_uploader     /home/$login/
-                          cp /mnt/.ydcmd.cfg                 /home/$login/
-                     sudo cp /mnt/.dropbox_uploader     /root
-                          cp -R /mnt/.ssh   /home/$login/
-                     sudo umount /dev/md0.eli
-                     sudo geli detach md0.eli
-                     sudo mdconfig -d -u 0 
-                          rm /tmp/passgeli
-                    cp -R chrome    /home/$login/.mozilla/firefox/iuvb8get.default
-nspluginwrapper -a -i
+  nspluginwrapper -a -i
 
 
-./geli.sh &           
+./geli.sh &&           
 
 
-cp -R chrome    /home/$login/.mozilla/firefox/iuvb8get.default
-~/scripts/fftor.sh & ./install.sh & 
+
+~/scripts/fftor.sh && ./install.sh && 
 
 git clone git://github.com/definitly/themes.git
 cp -R ~/themes/themes/* ~/.themes
