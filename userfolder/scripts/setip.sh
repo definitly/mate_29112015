abfabbc87a8ecba1855dae143acf769ec96b5f55
Your Name <you@example.com>
2015-11-28 13:01:47 +0300

    изменено:      userfolder/scripts/setip.sh

--- userfolder/scripts/setip.sh
+++ userfolder/scripts/setip.sh
@@ -3,3 +3,5 @@
 sudo ifconfig ale0 down
 
 sudo ifconfig ale0 192.168.1.33 netmask 255.255.255.0 up
+sudo service tor stop && sudo service tor start
+sudo service polipo stop && sudo service polipo start
